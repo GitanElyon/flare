fb62224ce2d91f444f73a508e1313a85cf160171
GitanElyon <gitanelyon@gmail.com>
2026-01-28 15:36:55 -0500

    added flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A feature rich customizable TUI app launcher written in Rust";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.default = pkgs.callPackage ./default.nix { };
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/tuig";
+      };
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          pkg-config
+        ];
+      };
+    }
+  );
+}
